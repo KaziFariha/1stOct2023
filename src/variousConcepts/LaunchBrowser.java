73ddcfcc8e73ab61f1cddbd89cb781d0076304ce
KaziFariha <135396584+KaziFariha@users.noreply.github.com>
2024-03-31 14:31:54 -0400

    Commited

---
+++ src/variousConcepts/LaunchBrowser.java
@@ -0,0 +1,20 @@
+package variousConcepts;
+
+import java.util.Scanner;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+// methods
+	// valiables/attiributes/field
+	public static void main (String[]args) {
+		
+		Scanner scn = new Scanner(System.in);
+		WebDriver driver = new ChromeDriver(); // pollymorphism
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		driver.get("https://codefios.com/ebilling/ ");
+		driver.close();
+	}
+}
